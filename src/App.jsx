030ea5c362cfa1091a6228ef0eae7f6b3435d12e
ktioru <mateuscst45@gmail.com>
2023-11-25 18:39:21 -0300

    Added minor corrections

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react'
 
 import './App.scss'
 
-import './counter'
 import Counter from './counter'
+
+import tomato from './assets/tomato.png'
+
+import githubIcon from './assets/github-icon.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,15 +15,15 @@ function App() {
   return (
     <div className='app'>
       <header>
+        <img src={tomato} alt="" />
         <h1>Pomodoro Timer</h1>
       </header>
       <main>
         <Counter time={25} name={"Work"}/>
         <Counter time={5} name={"Rest"}/>
-        
       </main>
       <footer>
-        <h1>Made by Ktioru</h1>
+        <h1>Made by <a href="https://github.com/Ktioru/" target="_blank" rel="noreferrer">Ktioru</a></h1><img src={githubIcon} width="64" height="64" />
       </footer>
     </div>
   )
